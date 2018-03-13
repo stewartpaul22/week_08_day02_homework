f7013a7f0752f34975ea3df0eafb64b7e724caf8
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-13 21:37:01 +0000

    Add test data in Runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,5 @@
 import db.DBHelper;
+import models.File;
 import models.Folder;
 
 public class Runner {
@@ -9,6 +10,15 @@ public class Runner {
         DBHelper.save(folder1);
         Folder folder2 = new Folder("projects");
         DBHelper.save(folder2);
+
+        File file1 = new File("day_01_homework", "java", 1056, folder1);
+        DBHelper.save(file1);
+        File file2 = new File("day_02_homework", "java", 876, folder1);
+        DBHelper.save(file2);
+        File file3 = new File("week_05_project_classes", "txt", 2876, folder2);
+        DBHelper.save(file3);
+        File file4 = new File("week_05_project_db", "java", 922, folder2);
+        DBHelper.save(file4);
 
 
 
