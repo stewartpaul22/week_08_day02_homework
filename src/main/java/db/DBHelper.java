428a73c3ba9b4b91eb00200a51df4872a426d1e8
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-13 20:18:05 +0000

    Add DBHelper.java

---
+++ src/main/java/db/DBHelper.java
@@ -0,0 +1,12 @@
+package db;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DBHelper {
+
+    private static Transaction transaction;
+    private static Session session;
+
+    
+}
